7dc793b2b44bcdab40d47d615117afabdf599125
Wang <maria.wang@capgemini.com>
2022-03-03 01:14:43 -0500

    msg

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -7,10 +7,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Application {
-
+	
+	@GetMapping("/message")
+	public String message() {
+		return "I am now deployed!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
